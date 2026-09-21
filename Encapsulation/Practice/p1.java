6e1507f53175aa5a0ba9bd549f31952033f0fb24
MoolaShashank <122132928+MoolaShashank@users.noreply.github.com>
2026-09-21 20:18:26 +0530

    Implement PiggyBank class with deposit and withdraw methods

---
+++ Encapsulation/Practice/p1.java
@@ -0,0 +1,29 @@
+public class PiggyBank {
+    private final String id;
+    private int savings;
+
+    public PiggyBank(String id) {
+        this.id = id;
+        this.savings = 0;
+    }
+
+    public void deposit(int amount) {
+        if (amount > 0) {
+            savings += amount;
+        }
+    }
+
+    public void withdraw(int amount) {
+        if (amount > 0 && amount <= savings) {
+            savings -= amount;
+        }
+    }
+
+    public int getSavings() {
+        return savings;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
